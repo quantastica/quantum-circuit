3798d40f3cf0ac4f4146f43f29240c951b44351c
Petar Korponaić <petar.korponaic@gmail.com>
2018-07-23 11:02:42 +0200

    Modified example.js

--- example.js
+++ example.js
@@ -3,17 +3,13 @@ var QuantumCircuit = require("./quantum-circuit.js");
 
 var circuit = new QuantumCircuit();
 
-circuit.addGate("h",  0, 1 );
-//circuit.addGate("x",  0, 1 );
-circuit.addGate("cx",  1, [0, 1] );
+circuit.addGate("h",  0, 1);
+circuit.addGate("cx",  1, [1, 0]);
 
-console.log(circuit.exportQASM("example"));
-
-//circuit.addGate("tdg",  0, 0);
-//circuit.addGate("h",  0, 1);
-//circuit.addGate("h",  0, 2);
-
+console.log(circuit.exportQASM("Bell state"));
 
 circuit.run();
+
+console.log("Final amplitudes:");
 
 circuit.print();
