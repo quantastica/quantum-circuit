9884e742f3b1b336b6d2b31b67402462da37fafa
Petar Korponaić <petar.korponaic@gmail.com>
2019-01-20 22:48:39 +0100

    Added classical control example

---
+++ example/nodejs/classical_control.js
@@ -0,0 +1,39 @@
+var QuantumCircuit = require("../../lib/quantum-circuit.js");
+
+var circ = new QuantumCircuit();
+
+circ.addGate("h", -1, 0);
+circ.addMeasure(0, "c", 0);
+circ.addGate("x", -1, 1, { 
+    condition: { 
+        creg: "c",
+        value: 1
+    }
+});
+
+circ.run();
+
+console.log("");
+console.log(circ.exportQASM());
+
+console.log("");
+console.log(circ.exportPyquil());
+
+//console.log("");
+//console.log(JSON.stringify(circ.exportRaw(), null, '\t'));
+
+console.log("");
+console.log("Final amplitudes:");
+circ.print(true);
+
+console.log("");
+console.log("Angles:");
+console.log(circ.angles());
+
+console.log("");
+console.log("Probabilities:");
+console.log(circ.probabilities());
+
+console.log("");
+console.log("Measured:");
+console.log(circ.measureAll());
